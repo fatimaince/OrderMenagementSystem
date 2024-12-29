8b988574d504691cd82c6d70daa689063bd4c6de
Fatıma ince <151375542+ffebruary12@users.noreply.github.com>
2024-12-29 22:56:51 +0300

    Update CustomLayoutManager.java

--- CustomLayoutManager.java
+++ CustomLayoutManager.java
@@ -18,10 +18,10 @@ import javax.swing.JButton;
 public class CustomLayoutManager {
 
     public void configureMainLayout(JTabbedPane tabbedPane, JPanel rightPanel, JFrame frame) {
-        // JSplitPane ile iki paneli ayırma
+        
         JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, tabbedPane, rightPanel);
-        splitPane.setDividerLocation(0.5); // Pencerenin %50'si tabbedPane, %50'si rightPanel olacak şekilde ayarla
-        splitPane.setEnabled(false); // JSplitPane'i sabit yapar, bölme çubuğunu hareket ettiremezsiniz
+        splitPane.setDividerLocation(0.5); 
+        splitPane.setEnabled(false); 
 
         frame.add(splitPane, BorderLayout.CENTER);
       
